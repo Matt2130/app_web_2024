2b4bb66427c16c829fc42f2ea2c9cb8f2cef672e
Matt2130 <169078016+Matt2130@users.noreply.github.com>
2024-11-29 18:34:55 -0600

    COMMIT P3 DJANGO V2

--- APP_WEB_2024/P3/practicasDjango/ProyectoUTDdjango/mainapp/urls.py
+++ APP_WEB_2024/P3/practicasDjango/ProyectoUTDdjango/mainapp/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from . import views
+from mainapp import views
 
 urlpatterns = [
     path('', views.index,name='inicio'),
     path('inicio/', views.index,name='inicio'),
-    path('acercade/', views.acercade,name='acercade'),
+    path('acercade/', views.about,name='about'),
     path('mision/', views.mision,name='mision'),
-    path('vision/', views.vision,name='vision')
+    path('vision/', views.vision,name='vision'),
+    path('registro/', views.register_user,name='registro'),
+    path('iniciosesion/', views.login_user,name='iniciosesion'),
+    path('logout/', views.logout_user,name='logout')
 ]
--- APP_WEB_2024/P3/practicasDjango/ProyectoUTDdjango/mainapp/views.py
+++ APP_WEB_2024/P3/practicasDjango/ProyectoUTDdjango/mainapp/views.py
@@ -1,33 +1,123 @@
-from django.shortcuts import render, HttpResponse, redirect
+from django.shortcuts import render,HttpResponse, redirect
+""" from django.contrib.auth.forms import UserCreationForm """
+from mainapp.forms import RegisterForm
+from django.contrib import messages
+from django.contrib.auth import authenticate , login, logout
+from django.contrib.auth.decorators import login_required
+
+
 
 # Create your views here.
 def index(request):
     return render(request,'mainapp/index.html',{
-        'tittle':'Inicio',
-        'content':'Bienvenido a mi pagina de incio',
+        'title':'Inicio',
+        'content':'||  Bienvenido a mi página de inicio  ||'
     })
-
-def acercade(request):
+@login_required(login_url='iniciosesion')
+def about(request):
     return render(request,'mainapp/about.html',{
-        'tittle':'Acerca de'
+        'title':'Acerca de',
+        'content':'Ayudamos a clientes a obtener una ventaja competitiva a través de la implementación de soluciones digitales que impulsan sus estrategias de negocio. Nuestra propuesta de valor se centra en las personas y se conecta con las necesidades de la organización y la tecnología.'
     })
 
+@login_required(login_url='iniciosesion')
 def mision(request):
-
     return render(request,'mainapp/mision.html',{
-        'tittle':'Mision',
+        'title':'Mision',
+        'content':'“Formar profesionistas altamente capacitados y comprometidos con el desarrollo sostenible, mediante una educación tecnológica de vanguardia que integra el conocimiento teórico y práctico. La Universidad Tecnológica de Durango busca contribuir al crecimiento económico, social y cultural de la región y del país, fomentando valores de responsabilidad, innovación y ética profesional, para enfrentar los retos de un mundo globalizado y en constante cambio.”'
     })
 
+@login_required(login_url='iniciosesion')
 def vision(request):
-    #HttpResponse(html)
     return render(request,'mainapp/vision.html',{
-        'tittle':'Vision',
+        'title':'Vision',
+        'content':'“Formar profesionistas altamente capacitados y comprometidos con el desarrollo sostenible, mediante una educación tecnológica de vanguardia que integra el conocimiento teórico y práctico. La Universidad Tecnológica de Durango busca contribuir al crecimiento económico, social y cultural de la región y del país, fomentando valores de responsabilidad, innovación y ética profesional, para enfrentar los retos de un mundo globalizado y en constante cambio.” '
     })
 
-#Redirigir a la página inicio con error 404    
-""" def redireccion_index(request, exception):
-    return redirect('inicio') """
+
+
+""" def articulos(request):
+    return render(request,'mainapp/articulos.html',{
+        'title':'Articulos',
+        'content':'.:: Aqui Van Los Articulos !::.'
+    })
+def categoria(request):
+    return render(request,'mainapp/categoria.html',{
+        'title':'categoria',
+        'content':'.:: Aqui Van Las Categorias !::.'
+    }) """
+
+
+def registro(request):
+    return render(request,'mainapp/registro.html',{
+        'title':'Registro',
+    })
+
+def inicioSesion(request):
+    return render(request,'mainapp/inicioSesion.html',{
+        'title':'Inicio de Sesión',
+    })
     
-#Redirigir a la página inicio con error 404, manera 2
-def redireccion_index_2(request,exception):
-    return render(request,'mainapp/404.html')
+""" def error404(request,exception):
+    return render(request,'errors/error404.html',{
+        'title':'Error 404',
+        'content':'''<h2>ERROR 404</h2>
+        </p>lO SENTIMOS NO PUDIMOS ENCONTRAR TU PAGINA <a class+="backhome" href={% url "mainapp/index.html" %}>regresar a inicio</a></p>'''
+    }) """
+    
+""" def error404(request, exception):
+    return redirect('inicio')
+    
+def error500(request):
+    return render(request, 'errors/error500.html', {
+        'title': 'Error 500',
+        'content': '''<h2>ERROR 500</h2>
+        <p>Ocurrió un error en el servidor. <a href="{% url 'inicio' %}">Regresar a inicio</a></p>'''
+    }) """
+    
+def error404(request,exception):
+    return render(request, 'errors/error404.html')
+
+def register_user(request):
+    if request.user.is_authenticated:
+        return redirect('inicio')
+    else:
+        register_form=RegisterForm()
+        
+        if request.method == "POST":
+            register_form = RegisterForm(request.POST)
+            
+            if register_form.is_valid():
+                register_form.save()
+                messages.success(request,'Te has registrado correctamente')
+                return redirect('inicio')
+                
+        return render(request,'users/registro.html',{
+            'title':'Registro',
+            'register_form':register_form
+        })
+        
+def login_user(request):
+    if request .user.is_authenticated:
+        return redirect('inicio')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user=authenticate(request,username=username,password=password)
+            
+            if user is not None:
+                login(request,user)
+                messages.success(request,'Bienvenido {} al inicio de sesión'.format(user.username))
+                return redirect('inicio')
+            else:
+                messages.warning(request,'No es posible iniciar sesión. Verifca tus creedenciales')
+            
+        return render(request,'users/inicioSesion.html',{
+        'title':'Inicio de Sesión',
+    })
+
+def logout_user(request):
+    logout(request)
+    return redirect('inicio')        
